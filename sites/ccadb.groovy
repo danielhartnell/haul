c4eb096b2e7bf2e5caefcf5710d55eb84d717593
Daniel Hartnell <danielhartnell@gmail.com>
2018-04-24 10:20:25 -0700

    addresses nokogiri installation error

--- sites/ccadb.groovy
+++ sites/ccadb.groovy
@@ -10,7 +10,7 @@ node {
   stage ('Build') {
     docker.image('starefossen/ruby-node:2-8').inside('-e https_proxy=$HTTPS_PROXY -e HTTPS_PROXY -e http_proxy=$HTTP_PROXY -e HTTP_PROXY') {
       sh "pwd"
-      sh "cd src && bundle install"
+      sh "cd src && bundle update"
       sh "cd src && bundle exec jekyll --version"
       sh "cd src && bundle exec jekyll build --verbose  -d ../dst"
     }
